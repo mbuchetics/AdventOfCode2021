c519508472489100ceeccba0c5023faf1ad3c4d8
Matthias Buchetics <mbuchetics@gmail.com>
2021-12-01 10:41:53 +0100

    day1 complete

--- AdventOfCode2021/Puzzles/Day1.swift
+++ AdventOfCode2021/Puzzles/Day1.swift
@@ -1,8 +1,52 @@
-//
-//  Day1.swift
-//  AdventOfCode2021
-//
-//  Created by Matthias Buchetics on 01.12.21.
-//
-
 import Foundation
+import Algorithms
+import Collections
+
+class Day1 {
+    
+    func part1() {
+        let input = day1_input.components(separatedBy: "\n")
+        
+        var increases = 0
+        let values = input.map { Int($0)! }
+        
+        for index in stride(from: 1, to: values.count, by: 1) {
+            let a = values[index]
+            let b = values[index - 1]
+            
+            if a > b {
+                increases += 1
+                print("\(a) (increased)")
+            } else if a < b {
+                print("\(a) (decreased)")
+            } else {
+                print("\(a) (no change)")
+            }
+        }
+        
+        print(increases)
+    }
+    
+    func part2() {
+        let input = day1_input.components(separatedBy: "\n")
+        
+        var increases = 0
+        let values = input.map { Int($0)! }
+        
+        for index in stride(from: 1, to: values.count - 2, by: 1) {
+            let sum1 = values[index ... index + 2].reduce(0, +)
+            let sum2 = values[index - 1 ... index + 1].reduce(0, +)
+            
+            if sum1 > sum2 {
+                increases += 1
+                print("\(sum1) (increased)")
+            } else if sum1 < sum2 {
+                print("\(sum1) (decreased)")
+            } else {
+                print("\(sum1) (no change)")
+            }
+        }
+        
+        print(increases)
+    }
+}
--- AdventOfCode2021/main.swift
+++ AdventOfCode2021/main.swift
@@ -9,3 +9,6 @@ import Foundation
 
 print("Hello, World!")
 
+let day1 = Day1()
+//day1.part1()
+day1.part2()
